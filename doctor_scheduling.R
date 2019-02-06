2e652fba859b414758dcfe37b20c50e3649cfc3a
abgr <andi.berger@yandex.com>
2019-02-06 16:29:07 +0100

    fixed small error

--- doctor_scheduling.R
+++ doctor_scheduling.R
@@ -1521,7 +1521,7 @@ optimal.schedule <- function(doctors = read.doctors(), requests = read.requests(
 		# TODO: normalize these somehow and put them on a quadratic function or something that punishes outliers
 		out1$optimization_factor <- (out1$opt_parms$n.unresolved + 0.01) ^ 4 *
 		                            (out1$opt_parms$n.requests_denied + 0.01) ^ 4 *
-		                            (1 - ifelse(out1$opt_parms$n.soft_requests == 0, 1, (out1$opt_parms$n.soft_requests_granted - 1) / out1$opt_parms$n.soft_requests)) ^ weights$soft_requests *
+		                            (1 - ifelse(out1$opt_parms$n.soft_requests == 0, 0.9, (out1$opt_parms$n.soft_requests_granted - 1) / out1$opt_parms$n.soft_requests)) ^ weights$soft_requests *
 		                            (max(0.5, out1$opt_parms$range.shifts) + 0.01) ^ weights$r.shifts *
 		                            (max(0.5, out1$opt_parms$range.weekends) + 0.01) ^ weights$r.weekends *
 		                            (max(1, out1$opt_parms$range.nights) + 0.01) ^ weights$r.nights *
