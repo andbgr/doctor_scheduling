8130343eb3d40624a9e424f9dc5338dbdc16f685
abgr <andi.berger@yandex.com>
2019-05-08 20:13:44 +0200

    rolled back usage of optimal.schedule2 function as it still has issues

--- example/2-create_schedule.R
+++ example/2-create_schedule.R
@@ -14,7 +14,7 @@ wards    <- input$wards
 # CREATE OPTIMAL SCHEDULE
 # this will take time, on my system (Core i3 from 2015) it takes 1h for about 7000 iterations
 system.time(
-out <- optimal.schedule2(doctors = doctors, requests = requests, wards = wards, 
+out <- optimal.schedule(doctors = doctors, requests = requests, wards = wards, 
                         n.iterations = 7000, weights = list(soft_requests = 1, 
                                                             r.shifts = 1, 
                                                             r.weekends = 1, 
